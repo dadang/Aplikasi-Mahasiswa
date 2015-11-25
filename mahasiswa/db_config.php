e5440af9616317e66f7766ca9305de0c283c386b
dadang <dadangiswan@gmail.com>
2015-11-25 17:05:22 +0700

    push aplikasi mahasiswa

---
+++ mahasiswa/db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "admin"); // db password
+define('DB_DATABASE', "aplikasi_mahasiswa"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
